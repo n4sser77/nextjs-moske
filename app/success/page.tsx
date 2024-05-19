36b372d84778a07d6b751277882fb707cfacec2b
n4sser77 <nasserqahtan0@gmail.com>
2024-05-19 23:20:08 +0200

    uppdated sucess page

--- app/success/page.tsx
+++ app/success/page.tsx
@@ -3,13 +3,21 @@ import ContactForm from "@/app/ui/components/ContactForm";
 export default function Kontakt() {
     return (
         <>
-        <hr></hr>
-         <h1 className="text-2xl text-slate-700 font-bold text-center mb-4">Skickat</h1>
-         <hr className="m-2 w-48 mx-auto border border-slate-200 border-2"/>
-
+            <hr></hr>
+            <h1 className="text-2xl text-slate-700 
+            font-bold text-center mb-4 "
+            >
+                Skickat
+            </h1>
+            <hr className="m-2 w-48 mx-auto 
+            border border-slate-200 border-2" 
+            />
+            <div className="mx-auto  
+            w-10/12 lg:mx-auto sm:w-10/12 lg:mx-auto 
+            sm:w-full lg:w-6/12"> Vi besvarar ditt medlande igenom kort</div>
 
         </>
-       
+
     );
 }
 
